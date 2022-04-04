f6f1627c5ff7b260da204a3c084288b249c04688
BillyBobBalding <ian_usmc@yahoo.com>
2022-04-04 17:13:12 -0500

    v.3 stopwatch working

--- timer.js
+++ timer.js
@@ -4,51 +4,32 @@ const fractional = document.getElementById('fractional');
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 
-startButton.addEventListener('click', formatTimer);
+startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
 
 let start;
 let interval;
 let count = 0;
 
-function formatTimer() {
+function startTimer() {
     start = Date.now();
     
     interval = setInterval(elapsedTime, 10)
-    
-    //seconds.textContent = start;
 }
 
 function elapsedTime() {
-    let elapsed = Date.now() - start;
-    fractional.textContent = elapsed;
-    seconds.textContent = Math.floor(elapsed / 1000);
+    let hasStarted = false;
 
-    //fractional.textContent = Math.round(elapsed / 1000);
-    //fractional.textContent = ((elapsed / 1000) % 1).toFixed(2);
+    
+    
+    let elapsed = Date.now() - start;
+    seconds.textContent = Math.floor(elapsed / 1000);
     fractional.textContent = ((elapsed / 1000) % 1).toFixed(2).toString().split('.')[1];
     
-    
-    // let fraction = elapsed;
-    // num % 100
-    // num / 10
-    // num % 10
+    //s2.textContent = elapsed;
 }
 
 function stopTimer() {
     clearInterval(interval);
     interval = null;
 }
-
-let ans = 1234 / 1000;
-// ans = Math.floor(ans / 100);
-// console.log(ans);
-
-// ans = 5678;
-// ans = Math.floor(ans / 100);
-// console.log(ans);
-
-// console.log(ans);
-console.log((ans % 1).toFixed(2));
-console.log(1234 % 10);
-console.log(1234 % 100);
